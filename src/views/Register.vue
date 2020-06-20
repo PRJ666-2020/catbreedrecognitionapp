9fb8d883cc6b14434a61a2bcaa4be8c55c87d27d
Erisli <42971850+Erisli@users.noreply.github.com>
2020-06-20 14:02:19 -0400

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,23 +1,26 @@
 <template>
-    <section class="section section-shaped section-lg my-0">
-        <div class="shape shape-style-1 bg-gradient-default">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div class="container pt-lg-md">
-            <div class="row justify-content-center">
-                <div class="col-lg-5">
-                    <card type="secondary" shadow
-                          header-classes="bg-white pb-5"
-                          body-classes="px-lg-5 py-lg-5"
-                          class="border-0">
-                        <template>
+  <section class="section section-shaped section-lg my-0">
+    <div class="shape shape-style-1 bg-gradient-default">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="container pt-lg-md">
+      <div class="row justify-content-center">
+        <div class="col-lg-5">
+          <card
+            type="secondary"
+            shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0"
+          >
+            <template>
               <ValidationObserver v-slot="{ handleSubmit }">
                 <form @submit.prevent="handleSubmit(onSubmit)">
                   <div class="form-group">
@@ -77,12 +80,12 @@
                 </form>
               </ValidationObserver>
             </template>
-                    </card>
-                </div>
-            </div>
-            <br>
+          </card>
         </div>
-    </section>
+      </div>
+      <br />
+    </div>
+  </section>
 </template>
 <script>
 import { minmax, required, confirmPassword } from "../validation";
