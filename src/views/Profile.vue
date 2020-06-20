96a58c1c39c9ffd9c808a011f75cd4ad116b1f14
Erisli <42971850+Erisli@users.noreply.github.com>
2020-06-20 17:18:31 -0400

    updated Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -19,46 +19,46 @@
                             <div class="col-lg-3 order-lg-2">
                                 <div class="card-profile-image">
                                     <a href="#">
-                                        <img v-lazy="'img/theme/team-4-800x800.jpg'" class="rounded-circle">
+                                        <img v-lazy="'img/theme/longhair.jpg'" class="rounded-circle">
                                     </a>
                                 </div>
                             </div>
                             <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
                                 <div class="card-profile-actions py-4 mt-lg-0">
-                                    <base-button type="info" size="sm" class="mr-4">Connect</base-button>
-                                    <base-button type="default" size="sm" class="float-right">Message</base-button>
+                                    <base-button type="default" size="sm" class="mr-4">Message</base-button>
+                                    <base-button type="info" size="sm" class="mr-4">Follow</base-button>  
+                                    <base-button type="default" size="sm" class="float-right">Edit</base-button>
                                 </div>
+                  
                             </div>
+                            
                             <div class="col-lg-4 order-lg-1">
                                 <div class="card-profile-stats d-flex justify-content-center">
+                                
                                     <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
+                                        <span class="heading">2</span>
+                                        <span class="description">Cat(s)</span>
                                     </div>
                                     <div>
                                         <span class="heading">10</span>
-                                        <span class="description">Photos</span>
+                                        <span class="description">Posts</span>
                                     </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
+                
                                 </div>
                             </div>
                         </div>
+                        
                         <div class="text-center mt-5">
-                            <h3>Jessica Jones
-                                <span class="font-weight-light">, 27</span>
-                            </h3>
+                            <h3>Jessica Jones</h3>
                             <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Bucharest, Romania</div>
                             <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
-                            <div><i class="ni education_hat mr-2"></i>University of Computer Science</div>
+                            <div><i class="ni education_hat mr-2"></i>Shorthair lover</div>
                         </div>
                         <div class="mt-5 py-5 border-top text-center">
                             <div class="row justify-content-center">
                                 <div class="col-lg-9">
-                                    <p>An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
-                                    <a href="#">Show more</a>
+                                    <p>Introduction</p>
+                                   
                                 </div>
                             </div>
                         </div>
