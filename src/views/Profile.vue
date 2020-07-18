ed6253aaa087bad9f5d1777589267b597b675da7
TaylorLi27 <T4ylor.li@gmail.com>
2020-07-18 12:15:12 -0400

    display user name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,75 +1,83 @@
 <template>
-    <div class="profile-page">
-        <section class="section-profile-cover section-shaped my-0">
-            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+  <div class="profile-page">
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+    <section class="section section-skew">
+      <div class="container">
+        <card shadow class="card-profile mt--300" no-body>
+          <div class="px-4">
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                    <img v-lazy="'img/theme/longhair.jpg'" class="rounded-circle" />
+                  </a>
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
+                <div class="card-profile-actions py-4 mt-lg-0">
+                  <base-button type="default" size="sm" class="mr-4">Message</base-button>
+                  <base-button type="info" size="sm" class="mr-4">Follow</base-button>
+                  <base-button type="default" size="sm" class="float-right">Edit</base-button>
+                </div>
+              </div>
+
+              <div class="col-lg-4 order-lg-1">
+                <div class="card-profile-stats d-flex justify-content-center">
+                  <div>
+                    <span class="heading">2</span>
+                    <span class="description">Cat(s)</span>
+                  </div>
+                  <div>
+                    <span class="heading">10</span>
+                    <span class="description">Posts</span>
+                  </div>
+                </div>
+              </div>
             </div>
-        </section>
-        <section class="section section-skew">
-            <div class="container">
-                <card shadow class="card-profile mt--300" no-body>
-                    <div class="px-4">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-3 order-lg-2">
-                                <div class="card-profile-image">
-                                    <a href="#">
-                                        <img v-lazy="'img/theme/longhair.jpg'" class="rounded-circle">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
-                                <div class="card-profile-actions py-4 mt-lg-0">
-                                    <base-button type="default" size="sm" class="mr-4">Message</base-button>
-                                    <base-button type="info" size="sm" class="mr-4">Follow</base-button>  
-                                    <base-button type="default" size="sm" class="float-right">Edit</base-button>
-                                </div>
-                  
-                            </div>
-                            
-                            <div class="col-lg-4 order-lg-1">
-                                <div class="card-profile-stats d-flex justify-content-center">
-                                
-                                    <div>
-                                        <span class="heading">2</span>
-                                        <span class="description">Cat(s)</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">Posts</span>
-                                    </div>
-                
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="text-center mt-5">
-                            <h3>Jessica Jones</h3>
-                            <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Bucharest, Romania</div>
-                            <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
-                            <div><i class="ni education_hat mr-2"></i>Shorthair lover</div>
-                        </div>
-                        <div class="mt-5 py-5 border-top text-center">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-9">
-                                    <p>Introduction</p>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </card>
+
+            <div class="text-center mt-5">
+              <h3>{{ userProfile.username }}</h3>
+              <!-- <div class="h6 font-weight-300">
+                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+              </div>
+              <div class="h6 mt-4">
+                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+              </div>
+              <div>
+                <i class="ni education_hat mr-2"></i>Shorthair lover
+              </div>-->
             </div>
-        </section>
-    </div>
+            <div class="mt-5 py-5 border-top text-center">
+              <div class="row justify-content-center">
+                <div class="col-lg-9">
+                  <p>Introduction</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["userProfile"])
+  }
+};
 </script>
 <style>
 </style>
